d7a004d535c0a54692c78045fede925dfe80a1f5
Jheison J. Mayta C <8401842+jmayta-dev@users.noreply.github.com>
2025-04-17 12:06:53 -0500

    💊 fix constraint syntax
    - fix primary key constraint declaration

--- universal/universal.currency.sql
+++ universal/universal.currency.sql
@@ -1,10 +1,9 @@
 -- -> CREATE TABLE CURRENCY
 CREATE TABLE IF NOT EXISTS Currency(
-      Id            INTEGER AUTOINCREMENT
-    , Name          TEXT NOT NULL
-    , PluralName    TEXT
-    , Code          TEXT NOT NULL
-    , Symbol        TEXT NOT NULL
-    , CONSTRAINT PK_Currency_Id PRIMARY KEY (Id)
+      Id         INTEGER PRIMARY KEY AUTOINCREMENT
+    , Name       TEXT NOT NULL
+    , PluralName TEXT
+    , Code       TEXT NOT NULL
+    , Symbol     TEXT
 );
 -- <- CREATE TABLE CURRENCY
--- universal/universal.uom.sql
+++ universal/universal.uom.sql
@@ -1,12 +1,11 @@
 -- -> CREATE TABLE UNIT OF MEASUREMENT
 CREATE TABLE IF NOT EXISTS UnitOfMeasurement(
-      Id              INTEGER AUTOINCREMENT
-    , Name            TEXT NOT NULL
-    , PluralName      TEXT NULL
-    , Abbreviation    TEXT NOT NULL
-    , NumericalValue  REAL CONSTRAINT DF_UnitOfMeasurement_NumericalValue DEFAULT 1.0
-    , IsActive        INTEGER
-    , BaseUnitId      INTEGER NULL
-    , CONSTRAINT PK_UnitOfMeasurement_Id PRIMARY KEY (Id)
+      Id             INTEGER PRIMARY KEY AUTOINCREMENT
+    , Name           TEXT NOT NULL
+    , PluralName     TEXT NULL
+    , Abbreviation   TEXT NOT NULL
+    , NumericalValue REAL CONSTRAINT DF_UnitOfMeasurement_NumericalValue DEFAULT 1.0
+    , IsActive       INTEGER
+    , BaseUnitId     INTEGER NULL
 );
 -- <- CREATE TABLE UNIT OF MEASUREMENT
